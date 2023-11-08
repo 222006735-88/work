415298bcf69588bd30893bd2873b5e21043a1f57
Naomi12coding <149513400+Naomi12coding@users.noreply.github.com>
2023-11-08 19:16:23 +0200

    Add files via upload

---
+++ A.java
@@ -0,0 +1,47 @@
+class A{
+    String msg = "Hello";{
+       System.out.println("Hello");
+    }}
+    class B{
+        String msg1 = "Welcome";
+        
+
+        { System.out.println("Welcome");}}
+
+        class C {
+            String msg2 = "Thank you";
+            public void msg2(){
+                System.out.println("Thank you");
+            }
+            public void msg1() {
+            }
+            public void msg() {
+            }}
+        // interface C implements B{
+            class main {
+               public static void main(String []args){
+
+                C obj = new C();
+                obj.msg2();
+                obj.msg1 ();
+                obj.msg();
+
+                // System.out.println(obj.msg2);
+                //obj.msg2();
+                //Sytem.out.println(obj.msg);
+                //obj.msg();
+             //System.out.println(obj.msg1);
+                //obj.msg1();
+
+
+            }}
+                
+            
+            
+        
+       
+        
+
+    
+    
+
